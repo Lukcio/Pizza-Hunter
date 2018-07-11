53e225e951ae2b0ebeb075bac16a7b1818b32ee4
Lucas Leschynski <mr.lukcio@gmail.com>
2018-07-11 13:24:01 -0500

    Added zooming to current location

--- 'Za Hunter/ViewController.swift
+++ 'Za Hunter/ViewController.swift
@@ -10,20 +10,32 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class ViewController: UIViewController {
-    @IBOutlet weak var mapView: MKMapView!
+class ViewController: UIViewController, CLLocationManagerDelegate {
     
+    @IBOutlet weak var mapView: MKMapView!
     let locationManager = CLLocationManager()
+    var region = MKCoordinateRegion()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager.requestWhenInUseAuthorization()
         mapView.showsUserLocation = true
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.first!
+        let center = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let span = MKCoordinateSpanMake(0.025, 0.025)
+        region = MKCoordinateRegionMake(center, span)
+        mapView.setRegion(region, animated: true)
     }
 
 
